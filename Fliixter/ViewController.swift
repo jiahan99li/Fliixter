7510a8eed3ad31ca32e7fd9ef1e89739d462a164
Jiahan Li <jiahanlijiang99@gmail.com>
2019-10-25 15:45:00 -0400

    Initial Commit

---
+++ Fliixter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Fliixter
+//
+//  Created by Jiahan Li on 10/25/19.
+//  Copyright © 2019 Jiahan Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
