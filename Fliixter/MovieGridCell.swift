c15da50ae6e3fad53a3fcd82364555960625c36a
Jiahan Li <jiahanlijiang99@gmail.com>
2019-10-25 21:42:14 -0400

    Commiting all files

---
+++ Fliixter/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Fliixter
+//
+//  Created by Jiahan Li on 10/25/19.
+//  Copyright © 2019 Jiahan Li. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
